fc8567d720b01b851140d7098cf60c0217e5ded3
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-11-26 14:03:14 +0000

    Green on player 2 winning. Now need to add in all winning conditions

--- lib/board.rb
+++ lib/board.rb
@@ -3,13 +3,32 @@ class Game
     @board = [[nil, nil, nil],[nil, nil, nil],[nil, nil, nil]]
     @column_map = { "A" => 0, "B" => 1, "C" => 2 }
     @row_map = { "1" => 0, "2" => 1, "3" => 2 }
+    @pieces = { "Player 1" => "O", "Player 2" => "X"}
+    # @Player 1 = "O"
+    # @Player 2 = "X"
+    @winner = nil
   end
 
   def player_1_move(move)
+    either_player_move(move, @pieces["Player 1"])
+  end
+
+  def player_2_move(move)
+    either_player_move(move, @pieces["Player 2"])
+  end
+
+  private
+
+  def either_player_move(move, player_piece)
     column = parse_column(move)
     row = parse_row(move)
-    @board[row][column] = "O"
-    return @board
+    @board[row][column] = player_piece
+    outcome_of_move(player_piece)
+  end
+
+  def outcome_of_move(player_piece)
+    check_winner(player_piece)
+    @winner ? "#{@winner} is the winner!" : @board
   end
 
   def parse_column(move)
@@ -19,4 +38,15 @@ class Game
   def parse_row(move)
     row = @row_map[move[1]]
   end
+
+  def check_winner(player_piece)
+    if @board[0][0] == @board[0][1] && @board[0][1] == @board[0][2] && @board[0][0]
+      @winner = @pieces.key(player_piece)
+    elsif @board[0][0] == @board[0][1] && @board[0][1] == @board[0][2] && @board[0][0]
+      @winner = "Player 2"
+    end
+  end
 end
+
+# All winners taken at line 44 because not checking for piece identity. Use
+# player_piece and backtrack through array?
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,4 +12,21 @@ describe Game do
       game.player_1_move("A1")
       expect(game.player_1_move("B1")).to eq(board)
     end
+    it 'lets player 2 enter an X' do
+      game = Game.new
+      board = [[nil, nil, "X"],[nil, nil, nil],[nil, nil, nil]]
+      expect(game.player_2_move("C1")).to eq(board)
+    end
+    it 'lets player 1 win' do
+      game = Game.new
+      game.player_1_move("A1")
+      game.player_1_move("B1")
+      expect(game.player_1_move("C1")).to eq("Player 1 is the winner!")
+    end
+    it 'lets player 2 win' do
+      game = Game.new
+      game.player_2_move("A1")
+      game.player_2_move("B1")
+      expect(game.player_2_move("C1")).to eq("Player 2 is the winner!")
+    end
 end
